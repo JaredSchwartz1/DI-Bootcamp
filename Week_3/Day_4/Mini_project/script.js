1d6dd758f63c40ba508fb66159b6d3829173f5d9
JaredSchwartz1 <jaredschwartz04@gmail.com>
2024-02-18 15:21:21 -0500

    big_progress on paint app

--- Week_3/Day_4/Mini_project/script.js
+++ Week_3/Day_4/Mini_project/script.js
@@ -1,32 +1,32 @@
 let section = document.getElementById('main');
 let colorTable = []
-let clearDiv=document.createElement('div')
+let clearDiv = document.createElement('div')
 let clearButton = document.getElementById('clear');
-let cursorColor=null
+let cursorColor = null
 
-for(i=0;i<27;i++){
-    let color=`rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`
-colorTable.push(color)
+for (i = 0; i < 27; i++) {
+    let color = `rgb(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})`
+    colorTable.push(color)
 }
-
 for (let i = 0; i <= 23; i++) {
     for (let j = 1; j <= 60; j++) {
         let paperTile = document.createElement('div')
         paperTile.classList.add('paperGrid')
         paperTile.setAttribute('id', `Tile${(i * 60 + j)}`)
-        paperTile.addEventListener('click',()=>{paintPaper(cursorColor)}) 
-        paperTile.addEventListener('mousedown',()=>{brushPaint(cursorColor)}) 
         section.appendChild(paperTile)
+        paperTile.addEventListener('mouseup', (ev) => { paintPaper(ev) })
+        // paperTile.addEventListener('mousedown', (ev) => { brushPaint(ev) })
     }
 }
-let paperTilesArray=document.getElementsByClassName('paperGrid')
+
+let paperTilesArray = document.getElementsByClassName('paperGrid')
 let aside = document.getElementById('sidebar');
 aside.appendChild(clearDiv)
 clearDiv.classList.add('clearDiv')
 clearDiv.appendChild(clearButton)
 for (let i = 1; i <= 7; i++) {
     for (let j = 1; j <= 3; j++) {
-        let setColor=colorTable[3 * (i - 1) + j-1]
+        let setColor = colorTable[3 * (i - 1) + j - 1]
         let colorTile = document.createElement('div')
         colorTile.classList.add('color')
         colorTile.setAttribute('id', `${setColor}`)
@@ -34,43 +34,72 @@ for (let i = 1; i <= 7; i++) {
         aside.appendChild(colorTile)
     }
 }
-for (i of document.getElementsByClassName('color')){
-    i.addEventListener('mouseover',highlight)
-    i.addEventListener('mouseout',unHighlight)
-   
+for (i of document.getElementsByClassName('color')) {
+    i.addEventListener('mouseover', highlight)
+    i.addEventListener('mouseout', unHighlight)
 }
 
-function highlight(){
-this.setAttribute('style',`background-color: ${this.getAttribute('id')};opacity: 0.5`)
-this.addEventListener('mouseclick',cursorColoring)
+function highlight() {
+    this.setAttribute('style', `background-color: ${this.getAttribute('id')};opacity: 0.5`)
+this.addEventListener('click', cursorColoring)
+    return
 }
-function unHighlight(){
-    this.setAttribute('style',`background-color: ${this.getAttribute('id')}`)
-}
-
-function cursorColoring(){
-this.setAttribute("style",`background-color: ${this.getAttribute('id')}; opacity: 0.5;border:3px solid gold`)
-cursorColor=this.getAttribute('id')
-return cursorColor
-}
-function cursorColoringCancel(cursorColor){
-    this.setAttribute("style",`background-color: ${cursorColor};`)
-cursorColor=null
+function unHighlight() {
+    this.setAttribute('style', `background-color: ${this.getAttribute('id')}`)
 }
 
-
-function paintPaper(cursorColor){
-    
-this.setAttribute('style',`background-color: ${cursorColor};`)
-}
-function brushPaint(cursorColor){
-
-}
-document.addEventListener('mousedblclick',()=>{cursorColoringCancel(cursorColor)})
-clearButton.addEventListener('click',resetAll)
-
-function resetAll(){
-    for(i of document.getElementsByClassName('paperGrid')){
-        i.setAttribute("style",`background-color: white;`)
+function cursorColoring() {
+    if (document.querySelector('.selectedColor') !== null) {
+        document.querySelector('.selectedColor').addEventListener('mouseover', highlight)
+        document.querySelector('.selectedColor').classList.remove('selectedColor')
+        this.classList.add('selectedColor')
     }
+    else {
+        this.classList.add('selectedColor')
+    }
+    this.setAttribute("style", `background-color: ${this.getAttribute('id')};`)
+    this.removeEventListener('mouseover', highlight)
+    // document.querySelector('*').addEventListener('mousedown', (ev) => { brushPaint(ev) })
+    cursorColor = this.getAttribute('id')
+    return cursorColor
+}
+function cursorColoringCancel() {
+    if (document.querySelector('.selectedColor') !== null) {
+        document.querySelector('.selectedColor').addEventListener('mouseover', highlight)
+        document.querySelector('.selectedColor').classList.remove('selectedColor')
+    }
+    cursorColor = null
+}
+
+document.querySelector('*').addEventListener('dblclick', () => { cursorColoringCancel(cursorColor) })
+clearButton.addEventListener('click', resetAll)
+
+function resetAll() {
+    for (i of document.getElementsByClassName('paperGrid')) {
+        i.setAttribute("style", `background-color: white;`)
+    }
+    if (document.querySelector('.selectedColor') !== null) {
+        document.querySelector('.selectedColor').addEventListener('mouseover', highlight)
+        document.querySelector('.selectedColor').classList.remove('selectedColor')
+    }
+    cursorColor = null
+}
+function paintPaper(ev) {
+    ev.currentTarget.setAttribute('style', `background-color: ${cursorColor};`)
+}
+function brushPaint(ev) {
+    
+    ev.currentTarget.addEventListener('mouseover', (ev) => {
+        while(ev.button==0){
+       ev.relatedTarget.setAttribute('style', `background-color: ${cursorColor};`)
+        }
+    })
+    ev.currentTarget.addEventListener('mouseup', (ev) => {
+        ev.currentTarget.removeEventListener('mouseover',(ev) => {
+            console.log(ev.button)
+            while(ev.button==0){
+           ev.relatedTarget.setAttribute('style', `background-color: ${cursorColor};`)
+            }
+        })
+     })
 }
