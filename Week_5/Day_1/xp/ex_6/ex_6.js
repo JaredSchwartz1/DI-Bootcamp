4b3058af2d47335325e350e4314ebedb9fe2e225
JaredSchwartz1 <jaredschwartz04@gmail.com>
2024-02-25 14:45:01 -0500

    done with xp too

--- Week_5/Day_1/xp/ex_6/ex_6.js
+++ Week_5/Day_1/xp/ex_6/ex_6.js
@@ -8,8 +8,7 @@ str=str.slice(1,str.length)
 str=(Number(str)+(Number(str)*vatIL))
 transactions[i]=transactions[i].slice(0,1).concat(str)
 console.log(transactions[i])
-console.log(`${bankAmount}+transactions[i].slice(0,1)+str`)
-bankAmount=Number(`${bankAmount}`.concat(`${transactions[i].slice(0,1)}`,`${transactions[i]}`))
+transactions[i].slice(0,1)=='+'?bankAmount+=str:bankAmount-=str
 console.log(bankAmount)
 })}
 
